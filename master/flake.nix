ebef68ae338ff11ac4ca316e33eeb4b006833be3
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:52 -0300

    chore: re-index ../nimpkgs/u/unrolled

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-unrolled-master.flake = false;
   inputs.src-unrolled-master.owner = "schneiderfelipe";
-  inputs.src-unrolled-master.ref   = "refs/heads/master";
+  inputs.src-unrolled-master.ref   = "master";
   inputs.src-unrolled-master.repo  = "unrolled";
   inputs.src-unrolled-master.type  = "github";
   
